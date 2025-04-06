73c3a186952986cf2ce494d15ea256f3aefddba0
Jiayi Yan <jiayi.yan0124@gmail.com>
2025-04-06 19:58:07 +0200

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,26 +1,29 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import os
 import random
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
-# 假设图片位于服务器的static文件夹，或使用公开URL替换
-IMAGE_FOLDER = './static/images/'
+IMAGE_FOLDER = os.path.join(app.static_folder, 'images')
 
 @app.route('/random-images', methods=['GET'])
 def random_images():
     all_images = os.listdir(IMAGE_FOLDER)
-    selected_images = random.sample(all_images, 140)
+    if len(all_images) < 140:
+        return jsonify({'error': 'Not enough images in the folder'}), 400
 
+    selected_images = random.sample(all_images, 140)
+    host = request.host_url.rstrip('/')
     response_data = {}
 
     for i in range(70):
         img1 = selected_images[2*i]
         img2 = selected_images[2*i + 1]
-        response_data[f'Q{i+1}_img1'] = f'https://你的服务器地址/static/images/{img1}'
-        response_data[f'Q{i+1}_img2'] = f'https://你的服务器地址/static/images/{img2}'
+        response_data[f'Q{i+1}_img1'] = f'{host}/static/images/{img1}'
+        response_data[f'Q{i+1}_img2'] = f'{host}/static/images/{img2}'
 
     return jsonify(response_data)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
