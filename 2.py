e5ef6dc5aa0f279cdf44b8a4b3a6f0d0d7d259ee
Jiayi Yan <jiayi.yan0124@gmail.com>
2025-04-06 20:22:22 +0200

    Update 2.py

--- 2.py
+++ 2.py
@@ -4,27 +4,39 @@ import random
 
 app = Flask(__name__)
 
+# 图片目录：直接放在根目录的 images 文件夹下
 IMAGE_FOLDER = os.path.join(os.getcwd(), 'images')
+
+@app.route('/')
+def index():
+    return '✅ Flask app is running! Visit /random-images to get image pairs.'
 
 @app.route('/random-images', methods=['GET'])
 def random_images():
-    all_images = os.listdir(IMAGE_FOLDER)
-    if len(all_images) < 140:
-        return jsonify({'error': 'Not enough images in the folder'}), 400
+    try:
+        all_images = os.listdir(IMAGE_FOLDER)
+        usable_images = [img for img in all_images if img.lower().endswith(('.jpg', '.png', '.jpeg'))]
+        
+        if len(usable_images) < 4:
+            return jsonify({'error': 'Too few images to generate pairs.'}), 400
 
-    selected_images = random.sample(all_images, 140)
-    host = request.host_url.rstrip('/')
-    response_data = {}
+        # 计算最大成对数量
+        pair_count = len(usable_images) // 2
+        selected_images = random.sample(usable_images, pair_count * 2)
+        host = request.host_url.rstrip('/')
+        response_data = {'total_pairs': pair_count}
 
-    for i in range(70):
-        img1 = selected_images[2*i]
-        img2 = selected_images[2*i + 1]
-        response_data[f'Q{i+1}_img1'] = f'{host}/images/{img1}'
-        response_data[f'Q{i+1}_img2'] = f'{host}/images/{img2}'
+        for i in range(pair_count):
+            img1 = selected_images[2*i]
+            img2 = selected_images[2*i + 1]
+            response_data[f'Q{i+1}_img1'] = f'{host}/images/{img1}'
+            response_data[f'Q{i+1}_img2'] = f'{host}/images/{img2}'
 
-    return jsonify(response_data)
+        return jsonify(response_data)
 
-# 添加这个路由：公开 images 文件夹
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
 @app.route('/images/<filename>')
 def serve_image(filename):
     return send_from_directory(IMAGE_FOLDER, filename)
