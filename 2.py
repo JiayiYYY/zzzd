e75ea106bd88399a4cb64df58e43539310e3b92c
Jiayi Yan <jiayi.yan0124@gmail.com>
2025-04-06 20:10:26 +0200

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,10 +1,10 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_from_directory
 import os
 import random
 
-app = Flask(__name__, static_folder='static')
+app = Flask(__name__)
 
-IMAGE_FOLDER = os.path.join(app.static_folder, 'images')
+IMAGE_FOLDER = os.path.join(os.getcwd(), 'images')
 
 @app.route('/random-images', methods=['GET'])
 def random_images():
@@ -19,10 +19,15 @@ def random_images():
     for i in range(70):
         img1 = selected_images[2*i]
         img2 = selected_images[2*i + 1]
-        response_data[f'Q{i+1}_img1'] = f'{host}/static/images/{img1}'
-        response_data[f'Q{i+1}_img2'] = f'{host}/static/images/{img2}'
+        response_data[f'Q{i+1}_img1'] = f'{host}/images/{img1}'
+        response_data[f'Q{i+1}_img2'] = f'{host}/images/{img2}'
 
     return jsonify(response_data)
+
+# 添加这个路由：公开 images 文件夹
+@app.route('/images/<filename>')
+def serve_image(filename):
+    return send_from_directory(IMAGE_FOLDER, filename)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))
